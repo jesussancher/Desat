746f60f1dda06fdc054bc7c2d8d562727d858fb0
jsancher <jsancher@ieee.org>
2020-05-25 10:34:29 -0500

    Mobile Navigation

--- js/nav.js
+++ js/nav.js
@@ -1,15 +1,73 @@
 $(document).ready(function(){
   $(".bars").on("click",function(){
 
-    if ($(navigation).height() == 0) {
+    if ($(navbox).height() == 0) {
+      
+      $(burger).animate({
+        opacity: '0%'
+    },"slow");
+
+      $(navbox).animate({
+          height: '380px',
+          width: '60%',
+          opacity: '100%'
+      },"1s");
+
       $(navigation).animate({
-          height: '180px'
+        height: '210px',
+        opacity: '100%'
       },"slow");
+
+      $(certi).animate({
+        opacity: '100%'
+      }, "slow");
+
     } else {
+
       $(navigation).animate({
-          height: '0px'
+          height: '0px',
+          opacity: '0%'
       },"slow");
+      
+      $(navbox).animate({
+        height: '0px',
+        width: '0%',
+        opacity: '0%'
+      },"slow");
+
+      $(certi).animate({
+        opacity: '0%'
+      }, "slow");
+
+      $(burger).animate({
+        opacity: '100%'
+      },"slow");
+    
     }
 
   });
+
+});
+
+$(document).ready(function(){
+$("header nav ul li a").on("click",function(){
+
+  if ($(navbox).height() > 0) {
+    
+    $(navigation).animate({
+        height: '0px'
+    },"slow");
+
+    $(navbox).animate({
+      height: '0px',
+      width: '0%'
+    },"slow");
+
+    $(burger).animate({
+      opacity: '100%'
+     },"slow");
+
+  } 
+
+});
 });
